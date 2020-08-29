3763978fa598754afddbda51480db8d4cd245c2d
Ashish Upadhyay <panditu2015@gmail.com>
2020-08-29 21:52:16 +0100

    Revert "from pc" from pc1 This reverts commit f23655111d29406378db214069f085797821c6f8.

--- model.py
+++ model.py
@@ -62,21 +62,3 @@ print(tmp_model)
 # mconf = GPTConfig(10, 10, n_layer=1, n_head=8, n_embd=512) # a GPT-1
 # model = GPT(mconf)
 # print(model)
-
-
-import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
-# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
-import logging
-logging.basicConfig(level=logging.INFO)
-
-# Load pre-trained model tokenizer (vocabulary)
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-
-# Encode a text inputs
-text = "Who was Jim Henson ? Jim Henson was a"
-indexed_tokens = tokenizer.encode(text)
-
-# Convert indexed tokens in a PyTorch tensor
-tokens_tensor = torch.tensor([indexed_tokens])

0178f350d26916ed1544a5f9a45d052119ff3776
Ashish Upadhyay <panditu2015@gmail.com>
2020-09-01 20:28:04 +0000

    gpt stuff

--- anno-gpt/ft.py
+++ anno-gpt/ft.py
@@ -98,8 +98,11 @@ def train(
 
             input_tensor = input_tensor.to(device)
             # outputs = model(input_tensor)
+            print("input_tensor.size", input_tensor.size())
             outputs = model(input_tensor, labels=input_tensor)
+            print("outputs.size", len(outputs))
             loss = outputs[0]
+            print("loss.size", loss.size())
             loss.backward()
 
             if (accumulating_batch_count % batch_size) == 0:
@@ -162,7 +165,7 @@ model_g_tr = train(
     # GPT2LMHeadModel.from_pretrained(gpt2_type),
     GPT2Tokenizer.from_pretrained(gpt2_type),
     batch_size=16,
-    epochs=1,
+    epochs=2,
     lr=3e-5,
     max_seq_len=140,
     warmup_steps=5000,
@@ -173,85 +176,85 @@ model_g_tr = train(
     save_model_on_epoch=True
 )
 
-def generate(
-    model,
-    tokenizer,
-    prompt,
-    entry_count=10,
-    entry_length=100,
-    top_p=0.8,
-    temperature=1.,
-):
+# def generate(
+#     model,
+#     tokenizer,
+#     prompt,
+#     entry_count=10,
+#     entry_length=100,
+#     top_p=0.8,
+#     temperature=1.,
+# ):
 
-    model.eval()
+#     model.eval()
 
-    generated_num = 0
-    generated_list = []
+#     generated_num = 0
+#     generated_list = []
 
-    filter_value = -float("Inf")
+#     filter_value = -float("Inf")
 
-    with torch.no_grad():
+#     with torch.no_grad():
 
-        for entry_idx in trange(entry_count):
+#         for entry_idx in trange(entry_count):
 
-            entry_finished = False
+#             entry_finished = False
 
-            generated = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
-            generated = generated.to("cuda")
+#             generated = torch.tensor(tokenizer.encode(prompt)).unsqueeze(0)
+#             generated = generated.to("cuda")
 
-            # Using top-p (nucleus sampling): https://github.com/huggingface/transformers/blob/master/examples/run_generation.py
+#             # Using top-p (nucleus sampling): https://github.com/huggingface/transformers/blob/master/examples/run_generation.py
 
-            for i in range(entry_length):
-                # outputs = model(generated)
-                outputs = model(generated, labels=generated)
-                loss, logits = outputs[:2]
-                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+#             for i in range(entry_length):
+#                 # outputs = model(generated)
+#                 outputs = model(generated, labels=generated)
+#                 loss, logits = outputs[:2]
+#                 logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
 
-                sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-                cumulative_probs = torch.cumsum(
-                    F.softmax(sorted_logits, dim=-1), dim=-1
-                )
+#                 sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+#                 cumulative_probs = torch.cumsum(
+#                     F.softmax(sorted_logits, dim=-1), dim=-1
+#                 )
 
-                sorted_indices_to_remove = cumulative_probs > top_p
-                sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
-                    ..., :-1
-                ].clone()
-                sorted_indices_to_remove[..., 0] = 0
+#                 sorted_indices_to_remove = cumulative_probs > top_p
+#                 sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+#                     ..., :-1
+#                 ].clone()
+#                 sorted_indices_to_remove[..., 0] = 0
 
-                indices_to_remove = sorted_indices[sorted_indices_to_remove]
-                logits[:, indices_to_remove] = filter_value
+#                 indices_to_remove = sorted_indices[sorted_indices_to_remove]
+#                 logits[:, indices_to_remove] = filter_value
 
-                next_token = torch.multinomial(F.softmax(logits, dim=-1), num_samples=1)
-                generated = torch.cat((generated, next_token), dim=1)
+#                 next_token = torch.multinomial(F.softmax(logits, dim=-1), num_samples=1)
+#                 generated = torch.cat((generated, next_token), dim=1)
 
-                if next_token in tokenizer.encode("<|endoftext|>"):
-                    entry_finished = True
+#                 if next_token in tokenizer.encode("<|endoftext|>"):
+#                     entry_finished = True
 
-                if entry_finished:
+#                 if entry_finished:
 
-                    generated_num = generated_num + 1
+#                     generated_num = generated_num + 1
 
-                    output_numpy = generated.squeeze().cpu()
-                    output_list = list(output_numpy)
+#                     output_numpy = generated.squeeze().cpu()
+#                     output_list = list(output_numpy)
 
-                    # output_list = list(generated.squeeze().numpy())
-                    output_text = tokenizer.decode(output_list)
+#                     # output_list = list(generated.squeeze().numpy())
+#                     output_text = tokenizer.decode(output_list)
 
-                    generated_list.append(output_text)
-                    break
+#                     generated_list.append(output_text)
+#                     break
             
-            if not entry_finished:
-                output_numpy = generated.squeeze().cpu()
-                output_list = list(output_numpy)
-                # output_list = list(generated.squeeze().numpy())
-                output_text = f"{tokenizer.decode(output_list)}<|endoftext|>" 
-                generated_list.append(output_text)
+#             if not entry_finished:
+#                 output_numpy = generated.squeeze().cpu()
+#                 output_list = list(output_numpy)
+#                 # output_list = list(generated.squeeze().numpy())
+#                 output_text = f"{tokenizer.decode(output_list)}<|endoftext|>" 
+#                 generated_list.append(output_text)
                 
-    return generated_list
+#     return generated_list
 
 
 
-generated_tweets = generate(model_g_tr, GPT2Tokenizer.from_pretrained(gpt2_type),"<|tweet|>",entry_count=10)
-# generated_tweets = generate(model.to('cpu'), GPT2Tokenizer.from_pretrained(gpt2_type),"<|tweet|>",entry_count=10)
+# generated_tweets = generate(model_g_tr, GPT2Tokenizer.from_pretrained(gpt2_type),"<|tweet|>",entry_count=10)
+# # generated_tweets = generate(model.to('cpu'), GPT2Tokenizer.from_pretrained(gpt2_type),"<|tweet|>",entry_count=10)
 
-print(generated_tweets)
+# print(generated_tweets)

2c22c128fc5cdceac91c73ae4e46eaa8e2fef788
Ashish Upadhyay <panditu2015@gmail.com>
2020-09-01 23:39:33 +0100

    added g2s dir

---
+++ g2s/model.py
@@ -0,0 +1,18 @@
+from g2s.gatenc import GAT
+from g2s.gptdec import GPT2
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+
+class Graph2Seq(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+
+def make_model():
+    pass
